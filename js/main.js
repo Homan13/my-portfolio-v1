0548bb644411fb0ed04774ff0afdab69a53c31ad
Homan13 <homan13@gmail.com>
2019-08-17 15:18:45 -0400

    Portfolio update to version 1.2.0 - New tests added to cover modals in test-example-work-modal.js - test-example-work.js updated to cover new unit tests for Example   Work - Example work bubbles turned into functioning modals

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "CDK Presentation",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "My AWS CDK MeetUp Presentation",
       'src': "images/meetUp.png",
@@ -13,6 +15,8 @@ const myWork = [
   },
   {
     'title': "Olive Branch Lodge Website",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Example screenshot of Olive Branch Lodge website",
       'src': "images/oliveBranchLodge_screenCap.png",
@@ -21,6 +25,8 @@ const myWork = [
   },
   {
     'title': "GitHub",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "My GitHub Profile",
       'src': "images/gitHub_screenCap.png",
